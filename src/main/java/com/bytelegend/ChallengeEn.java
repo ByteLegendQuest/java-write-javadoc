ce0abaafb3991a702fc5628b8dbc8e07d2fc9ac9
ProgrammeroftheWorld <1335197942@qq.com>
2022-01-03 17:26:55 +0000

    Update at 2022-01-03T17:26:55.551051Z

--- src/main/java/com/bytelegend/ChallengeEn.java
+++ src/main/java/com/bytelegend/ChallengeEn.java
@@ -16,9 +16,10 @@ public class ChallengeEn {
     /*
      * Read a text file with UTF-8 encoding and return its content.
      *
-     * Param filePath: path of the file to be read
-     * Return: the content string of the text file
-     * Throws IOException upon failures
+     * @param filePath: path of the file to be read
+     * @return the content string of the text file
+     * @throws IOException upon failures
+     * @author ProgrammeroftheWorld
      */
     public static String readUtf8Text(String filePath) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(filePath));
