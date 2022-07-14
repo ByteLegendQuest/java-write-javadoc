c9a15ce630355de89ae980f0b7bf72c483336347
tassellll <2112562745@qq.com>
2022-07-14 02:07:51 +0000

    Update at 2022-07-14T02:07:51.477832Z

--- src/main/java/com/bytelegend/ChallengeEn.java
+++ src/main/java/com/bytelegend/ChallengeEn.java
@@ -5,7 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-/*
+/** 
  * Convert all comments in this class to Javadoc with standard Javadoc tags: @author @see @return @throws
  * Feel free to put your own name into "author" tag, because you are the author of this code.
  *
@@ -13,7 +13,7 @@ import java.nio.file.Paths;
  * See: https://github.com/ByteLegendQuest/java-write-javadoc/blob/main/src/main/java/com/bytelegend/ChallengeEn.java
  */
 public class ChallengeEn {
-    /*
+    /**
      * Read a text file with UTF-8 encoding and return its content.
      *
      * Param filePath: path of the file to be read
