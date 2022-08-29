7f0403455e83e43606fd9d886edd28734fed4c2d
lltestjava <875716283@qq.com>
2022-08-29 13:24:12 +0000

    Update at 2022-08-29T13:24:12.084757Z

--- src/main/java/com/bytelegend/ChallengeZhHans.java
+++ src/main/java/com/bytelegend/ChallengeZhHans.java
@@ -5,21 +5,21 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-
-@param 请使用Javadoc的标准标签 @author @see @param @return @throws，把本类中的所有注释改写成Javadoc。
-@return 请勿修改注释内容本身，但是你可以把作者改写成你自己的名字，因为你当之无愧是本代码的作者之一。
-
-@author 作者：<lltestjava>
-@see See: https://github.com/ByteLegendQuest/java-write-javadoc/blob/main/src/main/java/com/bytelegend/ChallengeZhHans.java
- 
+/*
+ * 请使用Javadoc的标准标签 @author @see @param @return @throws，把本类中的所有注释改写成Javadoc。
+ * 请勿修改注释内容本身，但是你可以把作者改写成你自己的名字，因为你当之无愧是本代码的作者之一。
+ *
+ * @author 作者：<lltestjava>
+ * @see See: https://github.com/ByteLegendQuest/java-write-javadoc/blob/main/src/main/java/com/bytelegend/ChallengeZhHans.java
+ */
 public class ChallengeZhHans {
-    
-    @see 使用UTF-8编码读取一个文本文件并返回其内容
-    
-    @param 参数filePath：要读取的文件路径
-    @return 返回值：读取到的文本内容字符串
-    @throws 抛出IOException：如果无法读取
-    
+    /*
+     * 使用UTF-8编码读取一个文本文件并返回其内容
+     *
+     * @param 参数filePath：要读取的文件路径
+     * @return 返回值：读取到的文本内容字符串
+     * @throws 抛出IOException：如果无法读取
+     */
     public static String readUtf8Text(String filePath) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(filePath));
         return new String(bytes, StandardCharsets.UTF_8);
