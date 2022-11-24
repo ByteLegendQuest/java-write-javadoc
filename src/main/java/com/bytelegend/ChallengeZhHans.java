28c3519d310f3e9e8c43598e22f6c3efc924835c
ApoptosisToxin4869 <jiqianlang19660417@163.com>
2022-11-24 05:32:25 +0000

    Update at 2022-11-24T05:32:25.913704Z

--- src/main/java/com/bytelegend/ChallengeZhHans.java
+++ src/main/java/com/bytelegend/ChallengeZhHans.java
@@ -12,14 +12,24 @@ import java.nio.file.Paths;
  * 作者：<这里可以改成你自己的名字>
  * See: https://github.com/ByteLegendQuest/java-write-javadoc/blob/main/src/main/java/com/bytelegend/ChallengeZhHans.java
  */
+/**
+    @author : MayFly
+    @see :https://github.com/ByteLegendQuest/java-write-javadoc/blob/main/src/main/java/com/bytelegend/ChallengeZhHans.java
+ */
 public class ChallengeZhHans {
     /*
      * 使用UTF-8编码读取一个文本文件并返回其内容
      *
-     * 参数filePath：要读取的文件路径
+     * @paramter filePath：要读取的文件路径
      * 返回值：读取到的文本内容字符串
      * 抛出IOException：如果无法读取
      */
+
+     /**
+        @param filePath:要读取的文件路径
+        @return : 读取到的文本内容字符串
+        @throws :如果无法读取
+      */
     public static String readUtf8Text(String filePath) throws IOException {
         byte[] bytes = Files.readAllBytes(Paths.get(filePath));
         return new String(bytes, StandardCharsets.UTF_8);
