ff0ad084fe0e3afb78eea81b3d8eabe366edae49
mana1508 <mana150886@gmail.com>
2023-05-20 14:56:33 +0000

    Update at 2023-05-20T14:56:33.048353Z

--- src/main/java/com/bytelegend/ChallengeZhHans.java
+++ src/main/java/com/bytelegend/ChallengeZhHans.java
@@ -5,7 +5,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-/*
+/** 
  * 请使用Javadoc的标准标签 @author @see @param @return @throws，把本类中的所有注释改写成Javadoc。
  * 请勿修改注释内容本身，但是你可以把作者改写成你自己的名字，因为你当之无愧是本代码的作者之一。
  *
@@ -13,7 +13,7 @@ import java.nio.file.Paths;
  * See: https://github.com/ByteLegendQuest/java-write-javadoc/blob/main/src/main/java/com/bytelegend/ChallengeZhHans.java
  */
 public class ChallengeZhHans {
-    /*
+    /**
      * 使用UTF-8编码读取一个文本文件并返回其内容
      *
      * 参数filePath：要读取的文件路径
